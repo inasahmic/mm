4436e1062a1977963188282ed7bcd87e8d928c25
Inas Ahmić <36897946+inasahmic@users.noreply.github.com>
2025-09-04 17:17:12 +0200

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("")); // folder gdje ti je index.html
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
